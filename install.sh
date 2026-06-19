476cfbf829215ee0d01e092ec995e30e60013e4c
Muhammad Tsaqif Noor Az Zamil <azzamganteng921@gmail.com>
2026-06-19 15:40:52 +0700

    Update install.sh to indonesian and zzamcode branding

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ set -e
 
 ######################################################################################
 #                                                                                    #
-# Project 'pterodactyl-installer'                                                    #
+# Project 'ptero-install-zzamcode'                                                   #
 #                                                                                    #
 # Copyright (C) 2018 - 2026, Vilhelm Prytz, <vilhelm@prytznet.se>                    #
+# Modified by Zzamcode                                                               #
 #                                                                                    #
 #   This program is free software: you can redistribute it and/or modify             #
 #   it under the terms of the GNU General Public License as published by             #
@@ -21,14 +22,14 @@ set -e
 #   You should have received a copy of the GNU General Public License                #
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.           #
 #                                                                                    #
-# https://github.com/pterodactyl-installer/pterodactyl-installer/blob/master/LICENSE #
+# https://github.com/muhammadtsaqf/ptero-install-zzamcode/blob/main/LICENSE          #
 #                                                                                    #
 # This script is not associated with the official Pterodactyl Project.               #
-# https://github.com/pterodactyl-installer/pterodactyl-installer                     #
+# https://github.com/muhammadtsaqf/ptero-install-zzamcode                            #
 #                                                                                    #
 ######################################################################################
 
-export GITHUB_SOURCE="v1.3.0"
+export GITHUB_SOURCE="main"
 export SCRIPT_RELEASE="v1.3.0"
 export GITHUB_BASE_URL="https://raw.githubusercontent.com/muhammadtsaqf/ptero-install-zzamcode"
 
@@ -36,31 +37,31 @@ LOG_PATH="/var/log/pterodactyl-installer.log"
 
 # check for curl
 if ! [ -x "$(command -v curl)" ]; then
-  echo "* curl is required in order for this script to work."
-  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  echo "* curl sangat dibutuhkan agar skrip ini dapat berjalan."
+  echo "* silakan install menggunakan apt (Debian/Ubuntu) atau yum/dnf (CentOS/Rocky)"
   exit 1
 fi
 
 # Always remove lib.sh, before downloading it
 [ -f /tmp/lib.sh ] && rm -rf /tmp/lib.sh
-curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/master/lib/lib.sh
+curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/$GITHUB_SOURCE/lib/lib.sh
 # shellcheck source=lib/lib.sh
 source /tmp/lib.sh
 
 execute() {
-  echo -e "\n\n* pterodactyl-installer $(date) \n\n" >>$LOG_PATH
+  echo -e "\n\n* ptero-install-zzamcode $(date) \n\n" >>$LOG_PATH
 
   [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="main" && export SCRIPT_RELEASE="canary"
   update_lib_source
   run_ui "${1//_canary/}" |& tee -a $LOG_PATH
 
   if [[ -n $2 ]]; then
-    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    echo -e -n "* Instalasi $1 telah selesai. Apakah Anda ingin melanjutkan ke instalasi $2? (y/N): "
     read -r CONFIRM
     if [[ "$CONFIRM" =~ [Yy] ]]; then
       execute "$2"
     else
-      error "Installation of $2 aborted."
+      error "Instalasi $2 dibatalkan."
       exit 1
     fi
   fi
@@ -71,15 +72,15 @@ welcome ""
 done=false
 while [ "$done" == false ]; do
   options=(
-    "Install the panel"
+    "Install Panel"
     "Install Wings"
-    "Install both [0] and [1] on the same machine (wings script runs after panel)"
-    # "Uninstall panel or wings\n"
+    "Install keduanya [0] dan [1] di mesin yang sama (skrip wings berjalan setelah panel)"
+    # "Uninstall panel atau wings\n"
 
-    "Install panel with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install Wings with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install both [3] and [4] on the same machine (wings script runs after panel)"
-    "Uninstall panel or wings with canary version of the script (the versions that lives in master, may be broken!)"
+    "Install Panel dengan versi canary dari skrip (versi yang ada di main, mungkin ada bug!)"
+    "Install Wings dengan versi canary dari skrip (versi yang ada di main, mungkin ada bug!)"
+    "Install keduanya [3] dan [4] di mesin yang sama (skrip wings berjalan setelah panel)"
+    "Uninstall panel atau wings dengan versi canary (versi yang ada di main, mungkin ada bug!)"
   )
 
   actions=(
@@ -94,19 +95,19 @@ while [ "$done" == false ]; do
     "uninstall_canary"
   )
 
-  output "What would you like to do?"
+  output "Apa yang ingin Anda lakukan?"
 
   for i in "${!options[@]}"; do
     output "[$i] ${options[$i]}"
   done
 
-  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  echo -n "* Masukkan pilihan 0-$((${#actions[@]} - 1)): "
   read -r action
 
-  [ -z "$action" ] && error "Input is required" && continue
+  [ -z "$action" ] && error "Input diperlukan!" && continue
 
   valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
-  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Pilihan tidak valid!"
   [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
 done
 
